e17cab2c0d7156c2842b50ca7227e35915fd9cc4
sustainablelab <sustainablelab@gmail.com>
2018-05-22 09:23:57 -0400

    Temporary hack to link against mock-c and unity object files.

--- lib/build/link-o.md
+++ lib/build/link-o.md
@@ -1,1 +1,33 @@
 # Linking object files.
+
+The right way to link in the unit test libs I created in `mock-c`
+is to turn those `.o` files into a *Static Library* and link that
+library with `-llibrary`:
+> Search the library named library when linking.
+> 
+> The linker searches a standard list of directories for the
+> library, which is actually a file named liblibrary.a.  The linker
+> then uses this file as if it had been specified precisely by name.
+> The directories searched include several standard system
+> directories plus any that you specify with -L.
+> 
+> Normally the files found this way are library files---archive
+> files whose members are object files.  The linker handles an
+> archive file by scanning through it for members which define
+> symbols that have so far been referenced but not defined.
+
+The *Makefile* recipe lists `unity.o` as a prerequisite.
+My modification of `unity.c` gives `setUp` and `tearDown`
+*external linkage* and makes them function pointers.
+Even without including `unity.h` in this file,
+these function pointers must be defined *and* assigned.
+
+I define them globally:
+```c
+void (*setUp)(void); void (*tearDown)(void);
+```
+
+And inside `main()` I assign them to definitions:
+```c
+setUp = SetUp_Mock; tearDown = TearDown_Mock;
+```
--- lib/src/link-o.c
+++ lib/src/link-o.c
@@ -1,7 +1,66 @@
-#include <stdio.h>
-//#include <Mock.h>
+#include <stdio.h>         // printf
+#include <Mock.h>          // used by all test client code
+#include <RecordedCall.h>  // used by *DOF* `mock_lib.c`
+#include <RecordedArg.h>   // used by *DOF* `mock_lib.c`
+
+void (*setUp)(void); void (*tearDown)(void);
+void SetUp_Mock(void){ mock = Mock_new(); }
+void TearDown_Mock(void){ Mock_destroy(mock); mock = NULL; }
+Mock_s *mock;
 
 int main()
 {
+    setUp = SetUp_Mock; tearDown = TearDown_Mock;
+    setUp();
     printf("# Linking object files.\n");
+    printf(
+        "\nThe right way to link in the unit test libs I created in `mock-c`\n"
+        "is to turn those `.o` files into a *Static Library* and link that\n"
+        "library with `-llibrary`:\n"
+        "> Search the library named library when linking.\n"
+        "> \n"
+        "> The linker searches a standard list of directories for the\n"
+        "> library, which is actually a file named liblibrary.a.  The linker\n"
+        "> then uses this file as if it had been specified precisely by name.\n"
+        "> The directories searched include several standard system\n"
+        "> directories plus any that you specify with -L.\n"
+        "> \n"
+        "> Normally the files found this way are library files---archive\n"
+        "> files whose members are object files.  The linker handles an\n"
+        "> archive file by scanning through it for members which define\n"
+        "> symbols that have so far been referenced but not defined.\n"
+        "\nThe *Makefile* recipe lists `unity.o` as a prerequisite.\n"
+        "My modification of `unity.c` gives `setUp` and `tearDown`\n"
+        "*external linkage* and makes them function pointers.\n"
+        "Even without including `unity.h` in this file,\n"
+        "these function pointers must be defined *and* assigned.\n"
+        "\nI define them globally:\n"
+        "```c\n"
+        "void (*setUp)(void); void (*tearDown)(void);\n"
+        "```\n"
+        "\nAnd inside `main()` I assign them to definitions:\n"
+        "```c\n"
+        "setUp = SetUp_Mock; tearDown = TearDown_Mock;\n"
+        "```\n"
+        );
+    tearDown();
+}
+
+//=====[ sort includes with this client code example ]=====
+// These all go in the *DOF* `mock_lib.c`.
+// *Expect_DOF* and *DOF_Stubbed* prototypes go in the *DOF* `mock_lib.h`.
+static RecordedCall * Mock_TakesOneArg(uint8_t b1)
+{
+    char const *call_name = "TakesOneArg";
+    RecordedCall *record_of_call = RecordedCall_new(call_name);
+    RecordedArg *record_of_arg1 = RecordedArg_new(SetupRecord_uint8);
+    *((uint8_t *)record_of_arg1->pArg) = b1;
+    RecordArg(record_of_call, record_of_arg1);
+    return record_of_call;
+}
+void Expect_TakesOneArg(uint8_t b1) {
+    RecordExpectedCall(mock, Mock_TakesOneArg(b1));
+}
+void TakesOneArg_Stubbed(uint8_t b1) {
+    RecordActualCall(mock, Mock_TakesOneArg(b1));
 }
