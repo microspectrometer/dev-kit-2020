4939c58f763ffc20ae0957ca8e0809d2767ff9ab
sustainablelab <sustainablelab@gmail.com>
2020-05-16 22:23:43 -0400

    Script test-open.py generalizes printing replies to API commands

--- firmware/python/test-open.py
+++ firmware/python/test-open.py
@@ -2,8 +2,33 @@
 # -*- coding: utf-8 -*-
 '''Test hardware is visible.'''
 
-from chromaspeclib.simple import ChromaSpecSimpleInterface
-si = ChromaSpecSimpleInterface(serial_number='091103')
-print(si)
+# -----------
+# | Helpers |
+# -----------
+def _print_the_command_sent(reply):
+    print(f"Sent:\n\tCommand {reply.command_id}: {reply.name}")
+def _print_the_reply_received(reply):
+    received = reply.variables
+    print(f"Received:")
+    for response in received:
+        print(f"\t{response}: {getattr(reply, response)}")
+def report(reply):
+    _print_the_command_sent(reply)
+    _print_the_reply_received(reply)
 
+from chromaspeclib.simple import ChromaSpecSimpleInterface
+si = ChromaSpecSimpleInterface(
+    serial_number='091103', # dev-kit I'm using for my system tests
+    timeout=0.1 # timeout if there is no response in 100ms
+    )
+print(f"Using: {si}")
+
+# ----------------
+# | GetBridgeLED |
+# ----------------
+
+# Send the command and get the reply.
+reply = si.getBridgeLED(0)
+# Report the command and its reply.
+report(reply)
 
