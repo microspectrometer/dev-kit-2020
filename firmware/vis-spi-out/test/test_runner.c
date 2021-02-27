8deb4182dcaec376a9d29bb103484470ae703ad6
sustainablelab <sustainablelab@gmail.com>
2021-02-27 15:42:05 -0500

    Conditional include Lis/S13131 Configs.h

--- firmware/vis-spi-out/test/test_runner.c
+++ firmware/vis-spi-out/test/test_runner.c
@@ -1,11 +1,21 @@
 #include <unity.h>
 #include "test_example.h"
+#include "test_VisCmd.h"
+// Python-to-Firmware communication status codes
+#include "StatusCodes.h"
+// SENSOR configuration
+#ifdef LIS
+#include "LisConfigs.h"
+#endif
+#ifdef S13131
+#include "S13131Configs.h"
+#endif
+
 void (*setUp)(void); void (*tearDown)(void);
 void EmptySetUp(void){}
 void EmptyTearDown(void){}
-
-
 bool Yep=true, Nope=false; // Yep/Nope := tests on/off
+
 void test_example(bool run_test)
 {
     if (run_test)
@@ -16,9 +26,18 @@ void test_example(bool run_test)
         RUN_TEST(test_Can_call_inline_function_defined_in_app_lib);
     }
 }
+
+void test_VisCmd(bool run_test)
+{
+    if (run_test)
+    {
+        printf("bob");
+    }
+}
 int main(void)
 {
     UNITY_BEGIN();
     test_example(Yep);
+    test_VisCmd(Nope);
     return UNITY_END();
 }
