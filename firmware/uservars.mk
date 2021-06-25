74b7124163b3700bf38d277e0a45b55a9f56408e
sustainablelab <sustainablelab@gmail.com>
2021-06-25 13:51:51 -0400

    Remove uservars - build no longer requires path to ATmega DFP

--- firmware/uservars.mk
+++
@@ -1,8 +0,0 @@
-# Edit ATmega_DFP to match path of your installation.
-# Path is probably identical except for the version number.
-ATmega_DFP = /cygdrive/c/Program Files (x86)/Atmel/Studio/7.0/packs/atmel/ATmega_DFP/1.3.300/
-
-# atmega328_lib is the path to the
-# .o and .a lib files and the spec file.
-# This is probably the same in your installation.
-atmega328_lib = '$(ATmega_DFP)gcc/dev/atmega328p/'
